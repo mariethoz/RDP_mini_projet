2bac5654708b3525be550375c8976e6d709b7e02
Discretos <152215212+Discretos2022@users.noreply.github.com>
2024-12-18 15:51:48 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,30 +1,14 @@
+-- Table generation
+
 CREATE TABLE salle (
     id_salle SERIAL PRIMARY KEY,
-    name VARCHAR(255)
+    nom VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE table_a_manger (
     id_table SERIAL PRIMARY KEY,
     id_salle INT,
     taille INT
-);
-
-CREATE TABLE personnel (
-    id_personnel SERIAL PRIMARY KEY,
-    nom VARCHAR,
-    prenom VARCHAR,
-    type INT
-);
-
-CREATE TABLE personnel_table (
-    id_personnel_table SERIAL PRIMARY KEY,
-    id_personnel INT,
-    id_table INT
-);
-
-CREATE TABLE personnel_type (
-    id_personnel_type SERIAL PRIMARY KEY,
-    type VARCHAR
 );
 
 CREATE TABLE table_res (
@@ -36,55 +20,56 @@ CREATE TABLE table_res (
 CREATE TABLE reservation (
     id_reservation SERIAL PRIMARY KEY,
     id_client INT,
-    res_date DATE,
-    nb_personne INT
+    res_date DATE NOT NULL,
+    nb_personne INT CHECK (nb_personne > 0)
 );
 
 CREATE TABLE client (
     id_client SERIAL PRIMARY KEY,
-    nom VARCHAR,
-    prenom VARCHAR
+    nom VARCHAR(20) NOT NULL,
+    prenom VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE menu_carte (
-    id_menu_carte SERIAL PRIMARY KEY,
-    nom VARCHAR,
-    id_marge INT
+CREATE TABLE carte (
+    id_carte SERIAL PRIMARY KEY,
+    nom VARCHAR(40) NOT NULL,
+    id_marge INT,
+  	valide BOOLEAN DEFAULT FALSE 
 );
 
 CREATE TABLE recette (
     id_recette SERIAL PRIMARY KEY,
-    nom VARCHAR,
-    type VARCHAR
+    nom VARCHAR(40) NOT NULL,
+    recette_type VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE creation_menu_carte (
-    id_creation_menu_carte SERIAL PRIMARY KEY,
-    id_recette INT,
-    id_menu_carte INT
+CREATE TABLE subdivision_carte (
+    id_sub_carte SERIAL PRIMARY KEY,
+    id_carte INT,
+    id_recette INT
 );
 
 CREATE TABLE prend_menu (
     id_prend_menu SERIAL PRIMARY KEY,
     id_table INT,
-    id_menu_carte INT
+    id_carte INT
 );
 
 CREATE TABLE creation_recette (
     id_creation_recette SERIAL PRIMARY KEY,
     id_recette INT,
     id_ingredient INT,
-    quantite FLOAT,
-    ordre INT
+    quantite FLOAT CHECK (quantite > 0),
+    ordre INT CHECK (ordre > 0)
 );
 
 CREATE TABLE ingredient (
     id_ingredient SERIAL PRIMARY KEY,
-    nom VARCHAR,
+    nom VARCHAR(40) NOT NULL,
     prix_unitaire FLOAT
 );
 
 CREATE TABLE marge (
     id_marge SERIAL PRIMARY KEY,
-    valeur FLOAT
+    valeur FLOAT CHECK (valeur > 0)
 );
